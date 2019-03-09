be1f6a210de76a39871a2e38c9c4a71baef57f28
Timothy C. Siwula <tcsiwula@gmail.com>
2019-03-08 21:27:29 -0600

    :bowtie: [auto generated] via tim's deploy_script.sh

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -6,19 +6,6 @@ class Solution:
         # 3. if str[i] != str[j]
         #
 
-        i, j = 0, len(s) - 1
-
-        while i < j:
-            #
-            while not s[i].isalnum() and i < j:
-                i += 1
-            while not s[j].isalnum() and i < j:
-                j -= 1
-            if s[i].lower() != s[j].lower():
-                return False
-            i, j = i + 1, j - 1
-        return True
-
 
 # input: "babad"
 # expected: "aba"
---
+++ valid_palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+
+        while i < j:
+            #
+            while not s[i].isalnum() and i < j:
+                i += 1
+            while not s[j].isalnum() and i < j:
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i, j = i + 1, j - 1
+        return True
+
+# input: "A man, a plan, a canal: Panama"
+# output: true
+# expected: true
+
