6cf58866f97d63362e373312000bbd0cef376cad
Timothy C. Siwula <tcsiwula@gmail.com>
2019-10-29 00:28:52 -0500

    mbp local

---
+++ array_problems/squares_of_a_sorted_array.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+#
+
+class Solution(object):
+    def sortedSquares(self, A):
+        N = len(A)
+        # i, j: negative, positive parts
+        j = 0
+        while j < N and A[j] < 0:
+            j += 1
+        i = j - 1
+
+        ans = []
+        while 0 <= i and j < N:
+            if A[i]**2 < A[j]**2:
+                ans.append(A[i]**2)
+                i -= 1
+            else:
+                ans.append(A[j]**2)
+                j += 1
+
+        while i >= 0:
+            ans.append(A[i]**2)
+            i -= 1
+        while j < N:
+            ans.append(A[j]**2)
+            j += 1
+
+        return ans
